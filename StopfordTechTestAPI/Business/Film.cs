799e8c4d34d5b0786f5203256b292cf0eaae5671
Neil Butler <neilbutleranalytics@outlook.com>
2024-05-17 18:34:48 +0100

    Changes to the Film.cs code to check the validity of Rating, also added try/catch to make the code a little more robust. Also adding FilmList.cshtml as I forgot in the previous commit. Changes made here to add Rating to the Razor page.
    Also added screenshot of testing results

--- StopfordTechTestAPI/Business/Film.cs
+++ StopfordTechTestAPI/Business/Film.cs
@@ -10,21 +10,37 @@ namespace StopfordTechTestAPI.Business
         {
 
             List<FilmModel> fml = new List<FilmModel>();
-
-            using (var reader = new StreamReader(@"Data\FilmData.csv"))
+            try
             {
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
 
-                    FilmModel fm = new FilmModel();
-                    fm.FilmId = Convert.ToInt64(values[0]);
-                    fm.FilmName = values[1];
-                    fm.Tagline = values[2];
-                    fm.Rating = Convert.ToInt16(values[3]);
-                    fml.Add(fm);
+                using (var reader = new StreamReader(@"Data\FilmData.csv"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        FilmModel fm = new FilmModel();
+                        fm.FilmId = Convert.ToInt64(values[0]);
+                        fm.FilmName = values[1];
+                        fm.Tagline = values[2];
+                        // Check if the rating value is in a valid numeric format
+                        if (int.TryParse(values[3], out int rating))
+                        {
+                            fm.Rating = rating;
+                        }
+                        else
+                        {
+                            // If the rating is not in the correct format then default to 0
+                            fm.Rating = 0;
+                        }
+                        fml.Add(fm);
+                    }
                 }
+            }catch (Exception ex)
+            {
+                // Handle any exceptions that occur during reading or parsing
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
 
             return fml;
