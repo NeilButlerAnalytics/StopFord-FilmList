64a319840c724b8c31490622cd03a015a4657722
Neil Butler <neilbutleranalytics@outlook.com>
2024-05-17 18:55:30 +0100

    Change in code to add to the OnPost methodology.
    The solution should now be able to search for a film via the film name

--- StopfordTechTest/Pages/FilmList.cshtml.cs
+++ StopfordTechTest/Pages/FilmList.cshtml.cs
@@ -46,10 +46,40 @@ namespace StopfordTechTest.Pages
 
         public void OnPost(string search)
         {
-            
-            //search for name of film here
+            Task<string> jsonString = null; // To hold the JSON string fetched from the API
+            List<FilmModel> filmList = new List<FilmModel>(); // Empty list initialised to store the film data
 
-
+            using (var client = new HttpClient())
+            {
+                //Sends a GET request to the URL to fetch the list of films.
+                using (HttpResponseMessage response = client.GetAsync("https://localhost:7127/api/Film/ReturnFilmList").Result)
+                {
+                    // Only progress if request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            jsonString = content.ReadAsStringAsync(); // Read the response content
+                            filmList = JsonConvert.DeserializeObject<List<FilmModel>>(jsonString.Result); // Converts JSON string to FilmList
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("An error has occurred");
+                    }
+                }
+            }
+            // Just a User error check, if the user enters a search then we can display a search result
+            // But if no serach is specified then just assign FilmList.
+            // This ensures that the Films property always contains the appropriate data to display.
+            if (!string.IsNullOrEmpty(search))
+            {
+                Films = filmList.FindAll(f => f.FilmName.Contains(search, StringComparison.OrdinalIgnoreCase));
+            }
+            else
+            {
+                Films = filmList;
+            }
         }
     }
 }
